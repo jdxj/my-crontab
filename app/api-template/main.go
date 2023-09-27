c39b2b54691a67bb7feb88dc42768988822780b0
王友强 <wangyouqiang@fuya.live>
2023-09-27 20:43:46 +0800

    init: template

---
+++ app/api-template/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	_ "my-crontab/app/api-template/internal/packed"
+
+	"github.com/gogf/gf/v2/os/gctx"
+
+	"my-crontab/app/api-template/internal/cmd"
+)
+
+func main() {
+	cmd.Main.Run(gctx.GetInitCtx())
+}
